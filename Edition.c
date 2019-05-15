ae166ded680d527e7c3a2634ab5f84626424ee1c
BENKACI mounia <benkaci@informatique.univ-paris-diderot.fr>
2019-05-15 19:05:58 +0200

    correction d'erreur compilation

--- Edition.c
+++ Edition.c
@@ -1,7 +1,6 @@
 /* Module qui contient les fonctions copier, couper, coller */
 
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
@@ -10,17 +9,21 @@
 
 int copier(SDL_Texture* texture){
   if (texture == NULL){
-    printf("\nVeuillez d'abord selectionner une region de l'image");
+    printf("\nVeuillez d'abord selectionner une région de l'image");
     return 0;
   }
   return 1;
 }
 
 int coller(SDL_Window *window, SDL_Texture* texture, char* x, char *y, char *myl, char* myh){
-  
   SDL_Renderer* renderer = SDL_GetRenderer(window);
-  SDL_Rect* dst = {x, y, myl, myh};
-  SDL_RenderCopy(renderer, texture, NULL, dst);
+  SDL_Rect dst;
+  dst.x = atoi(x);
+  dst.y = atoi(y);
+  dst.w = atoi(myl);
+  dst.h = atoi(myh);
+   
+  SDL_RenderCopy(renderer, texture, NULL, &dst);
   SDL_RenderPresent(renderer);
   return 1;
 }

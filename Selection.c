2ae89bb63f45fc41e0848a9cdbfca8f8e59438f4
Amina26 <minassouane@gmail.com>
2019-05-16 04:30:08 +0200

    Corrections

--- Selection.c
+++ Selection.c
@@ -10,31 +10,25 @@ void colorSelect(SDL_Renderer *renderer, SDL_Rect *rect){
 
 
 void selectWindow(structWindow *myWindow){
-  printf("\nMamino");
+
   SDL_Surface *win_surface = myWindow->surface; 
   SDL_FillRect(win_surface, NULL, SDL_MapRGB(win_surface->format, 125,50,100));
-  //SDL_BlitSurface(win_surface, NULL, win_surface, NULL);  
+  SDL_BlitSurface(win_surface, NULL, win_surface, NULL);  
   SDL_UpdateWindowSurface(myWindow->window); 
   
   return;
 }
 
 void selectRect(structWindow *myWindow){
-  printf("\nAmina");
+  
   SDL_Surface *win_surface = myWindow->surface;
   int x = 10, y = 20, l = 200, h = 200;
   SDL_Rect dest_rect = {x, y, l, h}; 
   assert(SDL_FillRect(SDL_GetWindowSurface(myWindow->window), &dest_rect, SDL_MapRGB(win_surface->format, 125,50,100)) >= 0);
-  printf("\nFILLED it !");
   assert(SDL_BlitSurface(win_surface, NULL, win_surface, NULL) >= 0); 
   assert(SDL_UpdateWindowSurface(myWindow->window) >= 0);
-  printf("\nUPDATED");
   
-/*  SDL_Surface *win_surface = myWindow->surface; 
-  SDL_FillRect(win_surface, NULL, SDL_MapRGB(win_surface->format, 125,50,100));
-  //SDL_BlitSurface(win_surface, NULL, win_surface, NULL);  
-  SDL_UpdateWindowSurface(myWindow->window); */
-
+  printf("\nRectangle normalement dessiné");
   return;
 }
 
